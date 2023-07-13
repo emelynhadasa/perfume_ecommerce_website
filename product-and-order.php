fe3a563ba1263d6c259a982c4fd3b7fc4b2ce696
emelyndhadasa@gmail.com <emelyndhadasa@gmail.com>
2023-07-13 20:32:18 +0700

    first commit

---
+++ product-and-order.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Reed Diffusers &#124 Amerta</title> 
+        <link rel="stylesheet" href="style1.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <div class="header-container">
+            <p class="ltl-msg2"><span>&#9992;</span>Indonesia delivery in 5 to 10 business days <span>&#9829;</span></p>
+    
+            <nav class="nav-box2">
+                <table>
+                    <tr>
+                        <th class="logoabove"><img src="image/logopng.png" width="200px"></th>
+                        <td class="nav1"><a href="home-new.html"><img src="https://cdn-icons-png.flaticon.com/128/748/748015.png" width="18px"> Home</a></td>
+                        <td class="nav2"><a href="product-and-order.html"><img src="https://cdn-icons-png.flaticon.com/512/1872/1872598.png" width="20px"> Products & Order</a></td>
+                        <td class="nav3"><a href="contact-new.html"><img src="https://cdn-icons-png.flaticon.com/512/3884/3884261.png" width="16px"> Contact</a></td>
+                    </tr>
+                </table>
+            </nav>
+        </div>
+    </head>
+    
+    <body>
+        
+                <br><br>
+                <div class="form-container" style="margin-left: 300px; margin-top: 130px; margin-right: 300px; margin-bottom: 100px;">
+                    <h2>Order Here Cash On Delivery(COD):</h2>
+                    <form action="input_page.php"  method="post">
+                        <div class="mb-3">
+                          <label for="exampleInputEmail1" class="form-label">Name</label>
+                          <input type="name..." class="form-control" name="name" id="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email</label>
+                            <input type="email" class="form-control" name="email" id="email">
+                          </div>
+                        <div class="mb-3">
+                          <label for="exampleInputPassword1" class="form-label">Address</label>
+                          <input type="text" class="form-control" name="address" id="address">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Phone Number</label>
+                            <input type="text" class="form-control" name="phone_number" id="phone_number">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="inputGroupSelect01">Options</label>
+                            <select class="form-select" name="options" id="options">
+                                <option selected>Choose...</option>
+                                <option value="Sweet Vanilla">Sweet Vanilla</option>
+                                <option value="Coffee">Coffee</option>
+                                <option value="Jasmine">Jasmine</option>
+                            </select>
+                        </div>
+                            <button type="submit" value="Submit" class="btn btn-success" name="proses">
+                            </button>
+                    </form>
+                
+    
+    
+    </html>
+   
